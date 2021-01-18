9d0f6c1637fb38e7b3c682a38decfe9083fc487c
Dorian C B <dorianbehling@gmail.com>
2021-01-18 17:13:47 +0100

    First clean up of JSON to CSV

--- JSON_to_CSV.py
+++ JSON_to_CSV.py
@@ -5,13 +5,11 @@ alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', \
 
 with open('fictional_characters.csv', 'w', encoding = 'utf-8') as csv_file:
     with open('data_headers.txt') as data_headers:
-        csv_file.write('title, ontology/gender_label, ontology/gender, ontology/species_label, ontology/species, ontology/birthDate, ontology/birthYear, ontology/deathDate, ontology/deathYear, \
-            ontology/deathCause_label, ontology/deathCause, ontology/ethnicity_label, ontology/ethnicity, ontology/religion _label, ontology/religion \
-            ontology/nationality_label,ontology/nationality, ontology/country_label, ontology/country \
-            ontology/region_label, ontology/region, ontology/creator_label, ontology/creator')
+        csv_file.write('title, gender_label, gender, species_label, species, birthDate, birthYear, deathDate, deathYear, deathCause_label, deathCause, ethnicity_label, ethnicity, religion_label, religion, nationality_label, nationality, country_label, country, region_label, region, creator_label, creator\n')
     for i in range(26):
         with open(f'../People/{alphabet[i]}_people.json', encoding = 'utf-8') as json_file:
             people = json.load(json_file)
             for person in people:
                 if 'fiction' in str(person['http://www.w3.org/1999/02/22-rdf-syntax-ns#type_label']):
                     csv_file.write(f"{person['title']}, {person.get('ontology/gender_label')}, {person.get('ontology/gender')}, {person.get('ontology/species_label')}, {person.get('ontology/species')}, {person.get('ontology/birthDate')}, {person.get('ontology/birthYear')}, {person.get('ontology/deathDate')}, {person.get('ontology/deathYear')}, {person.get('ontology/deathCause_label')}, {person.get('ontology/deathCause')}, {person.get('ontology/ethnicity_label')}, {person.get('ontology/ethnicity')}, {person.get('ontology/religion _label')}, {person.get('ontology/religion')}, {person.get('ontology/nationality_label')}, {person.get('ontology/nationality')}, {person.get('ontology/country_label')}, {person.get('ontology/country')}, {person.get('ontology/region_label')}, {person.get('ontology/region')}, {person.get('ontology/creator_label')}, {person.get('ontology/creator')} \n")
+    # More code
---
+++ data_analysis.R
@@ -0,0 +1,4 @@
+library(tidyverse)
+setwd('C:/Users/User/Desktop/Uni/Semester 3/Winter lab course/12 Group Project/Develish-Data-Demons/')
+
+fictional_characters <- read_csv('fictional_characters.csv')
