0abb30af959376b5cda3e07efcff350d708c4c47
Dorian C B <dorianbehling@gmail.com>
2021-01-19 15:08:40 +0100

    Semi-working version of creator_data.csv maker

--- JSON_to_CSV.py
+++ JSON_to_CSV.py
@@ -1,4 +1,5 @@
 import json
+from os import readlink
 
 alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', \
 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -36,9 +37,11 @@ with open('fictional_characters.csv', 'w', encoding = 'utf-8') as csv_file:
                     deathyear = clean_data(person, 'deathYear')
                     deathcause = clean_data(person, 'deathCause')
                     ethnicity = clean_data(person, 'ethnicity')
+                    religion = clean_data(person, 'religion')
+                    nationality = clean_data(person, 'nationality')
                     country = clean_data(person, 'country')
                     region = clean_data(person, 'region')
-                    print(f'{title}, {gender}, {creator}')
-                    csv_file.write(f'{title}, {gender}, {creator}, {genre}, {firstappearance}, {species}, {birthdate}, {birthyear}, {deathdate}, {deathyear}, {deathcause}, {ethnicity}, {country}, {region}\n')
+                    print(f'{title}, {gender}, {creator}, {person.get("http://purl.org/dc/elements/1.1/description")}')
+                    #csv_file.write(f'{title}, {gender}, {creator}, {genre}, {firstappearance}, {species}, {birthdate}, {birthyear}, {deathdate}, {deathyear}, {deathcause}, {ethnicity}, {religion}, {nationality}, {country}, {region}\n')
                     #csv_file.write(f"{person['title']}, {person.get('ontology/gender_label')}, {person.get('ontology/gender')}, {person.get('ontology/species_label')}, {person.get('ontology/species')}, {person.get('ontology/birthDate')}, {person.get('ontology/birthYear')}, {person.get('ontology/deathDate')}, {person.get('ontology/deathYear')}, {person.get('ontology/deathCause_label')}, {person.get('ontology/deathCause')}, {person.get('ontology/ethnicity_label')}, {person.get('ontology/ethnicity')}, {person.get('ontology/religion _label')}, {person.get('ontology/religion')}, {person.get('ontology/nationality_label')}, {person.get('ontology/nationality')}, {person.get('ontology/country_label')}, {person.get('ontology/country')}, {person.get('ontology/region_label')}, {person.get('ontology/region')}, {person.get('ontology/creator_label')}, {person.get('ontology/creator')} \n")
     # More code
