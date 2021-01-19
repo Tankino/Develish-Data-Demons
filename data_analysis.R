2203f8decd2086395cf374d9dd9e80feba5b18d4
annie410 <77279469+annie410@users.noreply.github.com>
2021-01-19 17:30:29 +0100

    Delete data_analysis.R

--- data_analysis.R
+++
@@ -1,4 +0,0 @@
-library(tidyverse)
-setwd('C:/Users/User/Desktop/Uni/Semester 3/Winter lab course/12 Group Project/Develish-Data-Demons/')
-
-fictional_characters <- read_csv('fictional_characters.csv')
