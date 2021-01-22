3d12189696601795e46897dbbd6589e94fcb14e7
annie410 <angharad.macleod@gmail.com>
2021-01-22 16:40:38 +0100

    graphs larger font script commented

--- exploratory.R
+++ exploratory.R
@@ -4,18 +4,12 @@ library(tidyverse)
 library(ggthemes)
 
 creator_data <- read_csv("creator_data_cleaned.csv") 
+# loading the file which Dorian prepossessed in python
 
-genre_data <- count(creator_data, genre)
-
-creators_with_descriptions <- creator_data %>%
-  filter(!is.na(Description)) %>%
-  write_csv("creators_with_descriptions.csv")
-
-creators_with_genre <- creator_data %>%
-  filter(!is.na(genre))
-
-plot1a <- creator_data %>%
+with_decade_variable <- creator_data %>%
   select(birthYear, Number_of_characters) %>%
+# The following step was to code for a new variable indicating decade, which was done in 
+# a possible not super efficient way but it worked. Ctrl C and V were my friends.
   mutate(birthDecade = ifelse((birthYear<1500), "<=1400s", 0)) %>%
   mutate(birthDecade = ifelse((birthYear>1499) & (birthYear<1600), "1500s", birthDecade)) %>%
   mutate(birthDecade = ifelse((birthYear>1599) & (birthYear<1700), "1600s", birthDecade)) %>%
@@ -40,41 +34,51 @@ plot1a <- creator_data %>%
   mutate(birthDecade = ifelse((birthYear>1969) & (birthYear<1980), "1970s", birthDecade)) %>%
   mutate(birthDecade = ifelse((birthYear>1979) & (birthYear<1990), "1980s", birthDecade)) %>%
   mutate(birthDecade = ifelse((birthYear>1989) & (birthYear<2000), "1990s", birthDecade))
-  
-plot_SUM <- plot1a %>%
+
+plot_total_characters <- with_decade_variable %>%
   group_by(birthDecade) %>%
-  summarise(TOTcharacters_by_birthDecade = sum(Number_of_characters))
+  filter(!is.na(birthDecade)) %>%
+# to get rid of all the authors for whom we don't know the birth decade so they
+# don't get added to the chart too. In hindsight this could have been done before so 
+# as to not repeat for each plot. Oh well.
+  mutate(TOTcharacters_by_birthDecade = sum(Number_of_characters))
 
-plot_SUM <- plot_SUM[-c(25),]
+sum(plot_total_characters$Number_of_characters)
+# to know the total number of characters made by authors with birth date on Wikipedia
 
-ggplot(data=plot_SUM)+
+ggplot(data=plot_total_characters)+
   aes(x=birthDecade, y=TOTcharacters_by_birthDecade) +
   geom_col()+
   labs(x = "Birth decade of creator",
        y = "Total number of characters\n",
        caption = "n = 1927")+
   theme_economist_white(gray_bg = FALSE)+
-  scale_y_continuous(minor_breaks = seq(0 , 750, 50), breaks = seq(0, 700, 100), 
+  scale_y_continuous(breaks = seq(0, 700, 100), 
                      position = "right")+
-  theme(axis.text.x = element_text(angle = -45, hjust = 0.1, size = 14),
-        axis.text.y = element_text(vjust = -0.5, hjust = -10, size = 14),
-        axis.title=element_text(size=14),
-        panel.grid.minor = element_line(colour="grey", size=0.3),
-        panel.grid = element_line(colour="grey", size=0.3))
+  theme(axis.text.x = element_text(angle = -45, hjust = 0.1, size = 20),
+        axis.text.y = element_text(vjust = -0.5, hjust = -10, size = 20),
+        axis.title=element_text(size=20),
+        panel.grid = element_line(colour="grey", size=0.3),
+        plot.caption = element_text(size = 14))
 
 ggsave("total_characters.pdf")
 
-plot_AVG <- plot1a %>%
+plot_AVG <- with_decade_variable %>%
   group_by(birthDecade) %>%
   mutate(AVGcharacters_by_birthDecade = mean(Number_of_characters)) %>%
   add_count(birthDecade) %>%
   mutate(prop_contribution=Number_of_characters/n) %>%
+# this was to calculate the proportional contribution of each author to the average for that decade. 
   mutate(max_prop=ifelse(max(prop_contribution)==prop_contribution, prop_contribution, NA)) %>%
+# this was to keep only the highest proportional contribution
   filter(!is.na(max_prop)) %>%
   arrange(birthDecade) %>%
   filter(!is.na(birthDecade))
+# filtering out the unknowns
 
 plot_AVG <- plot_AVG[-c(12, 18, 22),]
+# this step was necessary because for 3 decades, there was an equally highest proportional contribution,
+# I removed the second manually. Probably a better way to do this automatically, but this worked too.
 
 ggplot(data=plot_AVG)+
   aes(x=birthDecade)+
@@ -99,13 +103,14 @@ ggplot(data=plot_AVG)+
 ggsave("average_characters.pdf")
 
 
-plot_AUTHORS <- plot1a %>%
+plot_total_authors <- with_decade_variable %>%
   group_by(birthDecade)%>%
-  arrange(birthDecade)%>%
   mutate(not_available = ifelse(birthYear == "NA", 1, 2))%>%
   subset(not_available==2)
+# here we have a different way to filter out those without birth year, leftover
+# from before I discovered is.na
 
-ggplot(data=plot_AUTHORS)+
+ggplot(data=plot_total_authors)+
   aes(x=birthDecade) +
   geom_bar()+
   labs(x = "Birth decade of creator",
@@ -113,11 +118,12 @@ ggplot(data=plot_AUTHORS)+
        caption = "n = 1927")+
   theme_economist_white(gray_bg = FALSE)+
   scale_y_continuous( position = "right")+
-  theme(axis.text.x = element_text(angle = -45, hjust = 0.1, size = 14),
-        axis.text.y = element_text(vjust = -0.5, hjust = -10, size = 14),
-        axis.title=element_text(size=14),
+  theme(axis.text.x = element_text(angle = -45, hjust = 0.1, size = 20),
+        axis.text.y = element_text(vjust = -0.5, hjust = -10, size = 20),
+        axis.title=element_text(size=20),
         panel.grid.minor = element_line(colour="grey", size=0.3),
-        panel.grid = element_line(colour="grey", size=0.3))
+        panel.grid = element_line(colour="grey", size=0.3),
+        plot.caption = element_text(size = 14))
 
 
 ggsave("total_authors.pdf")
